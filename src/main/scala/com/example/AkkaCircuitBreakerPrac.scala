b25dd36507b62ece245f195208f558c1c5ef0f26
petitviolet <violethero0820@gmail.com>
2016-10-17 01:08:27 +0900

    update

--- src/main/scala/com/example/AkkaCircuitBreakerPrac.scala
+++ src/main/scala/com/example/AkkaCircuitBreakerPrac.scala
@@ -9,18 +9,24 @@ import scala.concurrent.duration._
 
 object AkkaCircuitBreakerPrac extends App {
   private case class Message(value: String)
-  private case object UnsafeMessage
+  private case object PanicMessage
+  private case object HeavyMessage
 
   private class UnstableActor extends Actor {
     override def receive: Receive = {
       case Message(value) =>
-        println(value); sender ! s"receive: $value"
-      case UnsafeMessage =>
-        // sleep over callTimeout
-        Thread.sleep(1100); println("Oops...")
+        println(value)
+        sender ! s"receive: $value"
+      case PanicMessage =>
+        // just fail
+        sys.error("Oops...")
+      case HeavyMessage =>
+      // sleep over callTimeout
+        Thread.sleep(2000)
+        sender ! s"finish $HeavyMessage"
     }
   }
-  implicit val system =ActorSystem(s"AkkaCircuitBreakerPrac")
+  implicit val system = ActorSystem(s"AkkaCircuitBreakerPrac")
   implicit val dispatcher: ExecutionContext = ExecutionContext.Implicits.global
 
   val circuitBreaker = CircuitBreaker(
@@ -35,39 +41,42 @@ object AkkaCircuitBreakerPrac extends App {
   val actorRef = system.actorOf(Props[UnstableActor])
   implicit val timeout = Timeout(5.seconds)
 
+  // state is close
   circuitBreaker.withCircuitBreaker(actorRef ? Message("1"))
   circuitBreaker.withCircuitBreaker(actorRef ? Message("2"))
 
-  // force open
-  circuitBreaker.withCircuitBreaker(actorRef ? UnsafeMessage)
-  // state is close
-  // sleep over callTimeout
+  // failure once
+  circuitBreaker.withCircuitBreaker(actorRef ? PanicMessage)
+  // sleep over `callTimeout`
   Thread.sleep(1500)
-  // state is close
+  // state is still close
 
   circuitBreaker.withCircuitBreaker(actorRef ? Message("3"))
   // state is close
 
-  // force open with over maxFailure failures
-  circuitBreaker.withCircuitBreaker(actorRef ? UnsafeMessage)
-  circuitBreaker.withCircuitBreaker(actorRef ? UnsafeMessage)
+  // force open by over `maxFailure` failures
+  circuitBreaker.withCircuitBreaker(actorRef ? PanicMessage)
+  circuitBreaker.withCircuitBreaker(actorRef ? HeavyMessage)
+  // wait Thread.sleep at receive `HeavyMesage`
   Thread.sleep(3000)
   // state gets open
 
   // message goes dead-queue
   circuitBreaker.withCircuitBreaker(actorRef ? Message("4"))
-  // state is open
+  // state is still open
   Thread.sleep(3500)
-  // state gets half-open
+  // state gets half-open after `resetTimeout`
 
   circuitBreaker.withCircuitBreaker(actorRef ? Message("5"))
   // state gets close
 
-  // stil close
+  // still close
   Thread.sleep(1000)
   circuitBreaker.withCircuitBreaker(actorRef ? Message("6"))
   circuitBreaker.withCircuitBreaker(actorRef ? Message("7"))
   circuitBreaker.withCircuitBreaker(actorRef ? Message("8"))
+
+  // shutdown
   Thread.sleep(1000)
   system.terminate()
 }
