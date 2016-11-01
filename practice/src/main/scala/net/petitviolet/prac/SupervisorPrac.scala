04ee767341d6d84106200a0dfe4f815773ddca0f
petitviolet <violethero0820@gmail.com>
2016-11-02 08:14:21 +0900

    print future result

--- practice/src/main/scala/net/petitviolet/prac/SupervisorPrac.scala
+++ practice/src/main/scala/net/petitviolet/prac/SupervisorPrac.scala
@@ -1,5 +1,7 @@
 package net.petitviolet.prac
 
+import java.util.concurrent.ForkJoinPool
+
 import akka.actor.{Props, ActorSystem}
 import akka.pattern.ask
 import com.typesafe.config.ConfigFactory
@@ -10,7 +12,7 @@ import scala.concurrent.ExecutionContext
 
 object SupervisorPrac extends App {
   implicit val system = ActorSystem(s"SupervisorPrac")
-  implicit val dispatcher: ExecutionContext = ExecutionContext.Implicits.global
+  implicit val dispatcher: ExecutionContext = ExecutionContext.fromExecutor(new ForkJoinPool(1))
 
   val config = ConfigFactory.load().getConfig("supervisor")
 
@@ -19,39 +21,39 @@ object SupervisorPrac extends App {
   val actorRef = system.actorOf(Props[UnstableActor])
 
   // state is close
-  supervisorActor ! Execute(actorRef ? Message("1"))
-  supervisorActor ! Execute(actorRef ? Message("2"))
+  supervisorActor ? Execute(actorRef ? Message("1")) onComplete { case x => println(s"result =>$x") }
+  supervisorActor ? Execute(actorRef ? Message("2")) onComplete { case x => println(s"result =>$x") }
 
   // failure once
-  supervisorActor ! Execute(actorRef ? PanicMessage)
+  supervisorActor ? Execute(actorRef ? PanicMessage) onComplete { case x => println(s"result =>$x") }
   // sleep over `callTimeout`
   Thread.sleep(1500)
   // state is still close
 
-  supervisorActor ! Execute(actorRef ? Message("3"))
+  supervisorActor ? Execute(actorRef ? Message("3")) onComplete { case x => println(s"result =>$x") }
   // state is close
 
   // force open by over `maxFailure` failures
-  supervisorActor ! Execute(actorRef ? PanicMessage)
-  supervisorActor ! Execute(actorRef ? HeavyMessage)
+  supervisorActor ? Execute(actorRef ? PanicMessage) onComplete { case x => println(s"result =>$x") }
+  supervisorActor ? Execute(actorRef ? HeavyMessage) onComplete { case x => println(s"result =>$x") }
   // wait Thread.sleep at receive `HeavyMesage`
   Thread.sleep(3000)
   // state gets open
 
   // message goes dead-queue
-  supervisorActor ! Execute(actorRef ? Message("4"))
+  supervisorActor ? Execute(actorRef ? Message("4")) onComplete { case x => println(s"result =>$x") }
   // state is still open
   Thread.sleep(3500)
   // state gets half-open after `resetTimeout`
 
-  supervisorActor ! Execute(actorRef ? Message("5"))
+  supervisorActor ? Execute(actorRef ? Message("5")) onComplete { case x => println(s"result =>$x") }
   // state gets close
 
   // still close
   Thread.sleep(1000)
-  supervisorActor ! Execute(actorRef ? Message("6"))
-  supervisorActor ! Execute(actorRef ? Message("7"))
-  supervisorActor ! Execute(actorRef ? Message("8"))
+  supervisorActor ? Execute(actorRef ? Message("6")) onComplete { case x => println(s"result =>$x") }
+  supervisorActor ? Execute(actorRef ? Message("7")) onComplete { case x => println(s"result =>$x") }
+  supervisorActor ? Execute(actorRef ? Message("8")) onComplete { case x => println(s"result =>$x") }
 
   // shutdown
   Thread.sleep(1000)
